a6b69fc3b2ade364c23e33e0fb10489313065f92
Cory Lee <cl10001123@gmail.com>
2015-09-12 00:36:55 -0500

    Add Login server, and mongoDB

--- login/app/app/routes.js
+++ login/app/app/routes.js
@@ -13,10 +13,17 @@ module.exports = function(app, passport) {
     // =====================================
     // show the login form
     app.get('/login', function(req, res) {
-
         // render the page and pass in any flash data if it exists
         res.render('login.ejs', { message: req.flash('loginMessage') }); 
     });
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
 
     // process the login form
     // app.post('/login', do all our passport stuff here);
@@ -30,6 +37,12 @@ module.exports = function(app, passport) {
         // render the page and pass in any flash data if it exists
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/profile',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }));
 
     // process the signup form
     // app.post('/signup', do all our passport stuff here);
