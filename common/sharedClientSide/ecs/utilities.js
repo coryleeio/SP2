2f21fedabbc118ca4f7aee8e7fb4fb9aa6df55bb
Cory Lee <cl10001123@gmail.com>
2015-10-07 01:03:16 -0500

    Added algorithm to efficiently calculate possible compound keys

--- common/sharedClientSide/ecs/utilities.js
+++ common/sharedClientSide/ecs/utilities.js
@@ -22,11 +22,21 @@ module.exports = {
 		return compoundKey;
 	},
 
-	// returns an array containing the powerset of compoundKeys minus the empty set where 
+	// Returns an array containing the powerset of compoundKeys minus the empty set where 
     // each tuple is represented as a string with no spaces with each componentType appended in alphabetical order.
 	calculatePossibleCompoundKeys: function(componentTypes) {
-		// only support singular keys for now.
-		var sortedArray = componentTypes.sort();
-		return sortedArray;
+      outy = [];
+      var sortedArray = componentTypes.sort();
+      for(var i = 1; i < Math.pow(2,sortedArray.length); i++){
+        var compoundKeyArray = sortedArray.filter(function(element, index, array){
+            return (i & (1 << index));
+        });
+        var compoundKeyString = "";
+        compoundKeyArray.forEach(function(key){
+          compoundKeyString += key;
+        });
+        outy.push(compoundKeyString);
+      }
+      return outy;
 	}
 };
